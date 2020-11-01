d1f53e4adcd5cb3a9d19f8f5f711dc5dfc7388ee
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2020-11-01 12:39:57 -0500

    started product detail screen

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import { View, Text, ScrollView, Image, Button, StyleSheet } from 'react-native';
+
+const ProductDetailScreen = props =>{
+    return <View><Text>The detail Screen</Text></View>
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default ProductDetailScreen;
